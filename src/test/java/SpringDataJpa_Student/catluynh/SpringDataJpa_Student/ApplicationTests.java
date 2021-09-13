e04838f8c0df00e528f7d41be912b28a1559209c
catluynh <catluynh99@gmail.com>
2021-09-13 15:55:03 +0700

    create base spring data jpa

---
+++ src/test/java/SpringDataJpa_Student/catluynh/SpringDataJpa_Student/ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringDataJpa_Student.catluynh.SpringDataJpa_Student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
